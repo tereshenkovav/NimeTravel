28b5f342b1ac44ba2581cbcad3b36cf05e2dd2a9
TAV <tereshenkov@mail.ru>
2024-11-20 20:01:33 +0300

    Добавили в сборку MacOS создание DMG и получение версии из git.

--- setup/macos/build64.sh
+++ setup/macos/build64.sh
@@ -1,3 +1,18 @@
+for i in `git tag --list --sort=v:refname`; do BUILDTAG=$i; done
+
+for i in `git rev-parse HEAD`; do BUILDCOMMIT=$i; done
+BUILDCOMMIT=${BUILDCOMMIT:0:8}
+
+for i in `git rev-parse --abbrev-ref HEAD`; do BUILDBRANCH=$i; done
+
+echo $BUILDTAG $BUILDCOMMIT $BUILDBRANCH
+
+VERSION=${BUILDTAG:1}
+
+echo $BUILDTAG > ../../bin/text/version.txt
+echo $BUILDCOMMIT >> ../../bin/text/version.txt
+echo $BUILDBRANCH >> ../../bin/text/version.txt
+
 appdir=/tmp/NimeTravel.app
 mkdir $appdir
 mkdir $appdir/Contents
@@ -33,7 +48,9 @@ cp -R /Library/Frameworks/vorbisfile.framework $appdir/Contents/Frameworks
 cd /tmp 
 
 echo en > $appdir/Contents/MacOS/text/default
-zip -r NimeTravel-prologue-EN-1.1.0-MacOS.app.zip NimeTravel.app
+zip -r9 NimeTravel-prologue-EN-$VERSION-MacOS.app.zip NimeTravel.app
+hdiutil create -srcfolder $appdir -volname "NimeTravel" -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDZO -size 30000k -imagekey zlib-level=9 NimeTravel-EN-$VERSION-MacOS.dmg
 
 echo ru > $appdir/Contents/MacOS/text/default
-zip -r NimeTravel-prologue-RU-1.1.0-MacOS.app.zip NimeTravel.app
+zip -r9 NimeTravel-prologue-RU-$VERSION-MacOS.app.zip NimeTravel.app
+hdiutil create -srcfolder $appdir -volname "NimeTravel" -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDZO -size 30000k -imagekey zlib-level=9 NimeTravel-RU-$VERSION-MacOS.dmg
